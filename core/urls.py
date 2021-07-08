0b037dd4bd8b111b4040f87cd0b9c94d9dbb374b
Henry Buisseret <henrybuisseret@gmail.com>
2021-07-08 21:23:58 +0200

    templates management before part2

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,22 @@
 from django.urls import path
-from .views import item_list
+from .views import home, products, checkout
+# from .views import (
+#     ItemDetailView,
+#     CheckoutView,
+#     HomeView,
+#     OrderSummaryView,
+#     add_to_cart,
+#     remove_from_cart,
+#     remove_single_item_from_cart,
+#     PaymentView,
+#     AddCouponView,
+#     RequestRefundView
+# )
 
 app_name = 'core'
 
 urlpatterns = [
-    path('', item_list, name='item-list')
+    path('', home, name='item-list'),
+    path('checkout/', checkout, name='checkout'),
+    path('product/', products, name='product'),
 ]
