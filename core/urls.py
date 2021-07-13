ba011e5ab0690b3b4fe47a21934c0378cb702e34
Henry Buisseret <henrybuisseret@gmail.com>
2021-07-13 18:26:21 +0200

    partie 5 1/3

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import products, checkout
+from .views import products
 from .views import (
-    checkout,
     ItemDetailView,
-    # CheckoutView,
+    CheckoutView,
     HomeView,
     OrderSummaryView,
     add_to_cart,
@@ -18,7 +17,7 @@ app_name = 'core'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='item-list'),
-    path('checkout/', checkout, name='checkout'),
+    path('checkout/', CheckoutView.as_view(), name='checkout'),
     path('order-summary/', OrderSummaryView.as_view(), name='order-summary'),
     path('product/<slug>/', ItemDetailView.as_view(), name='product'),
     path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
