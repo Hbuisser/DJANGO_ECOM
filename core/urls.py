063ff303caa13483f608dd72347cc8e80556d430
Henry Buisseret <henrybuisseret@gmail.com>
2021-07-13 16:51:21 +0200

    partie 3 ok

--- core/urls.py
+++ core/urls.py
@@ -4,11 +4,11 @@ from .views import (
     checkout,
     ItemDetailView,
     # CheckoutView,
-     HomeView,
-    # OrderSummaryView,
+    HomeView,
+    OrderSummaryView,
     add_to_cart,
     remove_from_cart,
-    # remove_single_item_from_cart,
+    remove_single_item_from_cart,
     # PaymentView,
     # AddCouponView,
     # RequestRefundView
@@ -19,7 +19,9 @@ app_name = 'core'
 urlpatterns = [
     path('', HomeView.as_view(), name='item-list'),
     path('checkout/', checkout, name='checkout'),
+    path('order-summary/', OrderSummaryView.as_view(), name='order-summary'),
     path('product/<slug>/', ItemDetailView.as_view(), name='product'),
     path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
     path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart'),
+    path('remove-item-from-cart/<slug>/', remove_single_item_from_cart, name='remove-single-item-from-cart'),
 ]
